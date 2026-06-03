b3b564082f4356349ebe31e96ee9b9c309691300
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-06-03 10:11:31 +0300

    Create Directive.php

---
+++ src/Views/Contracts/Directive.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace M2Collective\PackageTools\Views\Contracts;
+
+interface Directive
+{
+    /**
+     * @return string
+     */
+    public function openingTag() : string;
+
+    /**
+     * @param mixed $expression
+     * @return string
+     */
+    public function openingHandler(mixed $expression) : string;
+
+    /**
+     * @return string
+     */
+    public function closingTag() : string;
+
+    /**
+     * @param mixed $expression
+     * @return string
+     */
+    public function closingHandler(mixed $expression) : string;
+
+    /**
+     * @return string
+     */
+    public function logicalTag() : string;
+
+    /**
+     * @param mixed $expression
+     * @return string
+     */
+    public function logicalHandler(mixed $expression) : string;
+}
