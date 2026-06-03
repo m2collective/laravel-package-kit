328e7bb2ddb383263ecc287f8a27b2b773b790c1
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-06-03 10:11:28 +0300

    Create RegisterDirectivesTrait.php

---
+++ src/Supports/Traits/RegisterDirectivesTrait.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace M2Collective\PackageTools\Supports\Traits;
+
+use Illuminate\Support\Facades\Blade;
+use M2Collective\PackageTools\Views\Contracts\Directive;
+
+trait RegisterDirectivesTrait
+{
+    /**
+     * @param array $directives
+     * @param bool $registering
+     * @return void
+     */
+    private function registerDirectives(array $directives, bool $registering = true) : void
+    {
+        if($directives !== [] && $registering) {
+            foreach ($directives as $directive) {
+                $this->registerDirective($directive);
+            }
+        }
+    }
+
+    /**
+     * @param Directive $directive
+     * @return void
+     */
+    private function registerDirective(Directive $directive) : void
+    {
+        Blade::directive($directive->openingTag(), [$directive, 'openingHandler']);
+        Blade::directive($directive->logicalTag(), [$directive, 'logicalHandler']);
+        Blade::directive($directive->closingTag(), [$directive, 'closingHandler']);
+    }
+}
