c245d83a18e32172bbe214489d79c0f0f90748a0
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-06-03 15:44:13 +0300

    Fix Namespace

--- src/View/Contracts/SimpleDirective.php
+++ src/View/Contracts/SimpleDirective.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace M2Collective\PackageKit\View\Contracts;
 
+use M2Collective\PackageKit\View\Contracts\Directives\CustomOpeningTag;
+
 interface SimpleDirective extends BaseDirective, CustomOpeningTag
 {
 
