447db452b32c6c19fe5b297cc380186be83e14e0
bu0nq <67270989+bu0nq@users.noreply.github.com>
2026-06-07 14:20:58 +0300

    Fix

---
+++ src/Support/Providers/AggregateServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace M2Collective\PackageKit\Support\Providers;
+
+use Illuminate\Support\AggregateServiceProvider as ServiceProvider;
+
+abstract class AggregateServiceProvider extends ServiceProvider
+{
+
+}
